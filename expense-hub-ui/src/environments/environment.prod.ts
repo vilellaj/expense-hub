3d823609bde6e4edb20ced97186bc799b6cf6770
Jonathan Vilella <vilellaj@hotmail.com>
2020-03-18 09:25:22 -0300

    feat(build): Setup dockercompose

--- expense-hub-ui/src/environments/environment.prod.ts
+++ expense-hub-ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://expense-hub-service/api/v1',
+  apiUrl: 'http://localhost:3000/api/v1',
   storageKeys: {
     session: 'eh-session'
   }
